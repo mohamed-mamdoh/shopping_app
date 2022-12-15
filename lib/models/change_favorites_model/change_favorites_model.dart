cc6949f1a51ab94f3e31ad7852e2da88db9f4542
mohamed <moamedmedo55@gmail.com>
2022-12-15 17:59:52 +0200

    finally

---
+++ lib/models/change_favorites_model/change_favorites_model.dart
@@ -0,0 +1,74 @@
+
+class ChangeFavoritesModel{
+  bool? status;
+  String? message;
+  Data? data;
+  ChangeFavoritesModel({this.status,this.message,this.data});
+
+  ChangeFavoritesModel.fromJson(Map<String,dynamic> json)
+  {
+    status=json['status'];
+    message=json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data?.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  int? id;
+  Product? product;
+
+  Data({this.id, this.product});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    product =
+    json['product'] != null ? Product.fromJson(json['product']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    if (product != null) {
+      data['product'] = product?.toJson();
+    }
+    return data;
+  }
+}
+
+class Product {
+  int? id;
+  dynamic price;
+  dynamic oldPrice;
+  int? discount;
+  String? image;
+
+  Product({this.id, this.price, this.oldPrice, this.discount, this.image});
+
+  Product.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    price = json['price'];
+    oldPrice = json['old_price'];
+    discount = json['discount'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['price'] = price;
+    data['old_price'] = oldPrice;
+    data['discount'] = discount;
+    data['image'] = image;
+    return data;
+  }
+}
--- lib/modules/shop_app/setting/setting_screen.dart
+++ lib/modules/shop_app/setting/setting_screen.dart
@@ -1,17 +1,129 @@
 
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shopping_app/models/cubit_shop/cubit.dart';
+import 'package:shopping_app/models/cubit_shop/states.dart';
+import 'package:shopping_app/shared/components/components.dart';
+import 'package:shopping_app/shared/components/constans.dart';
 
 
 class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+  var formKey=GlobalKey<FormState>();
+  var nameController=TextEditingController();
+  var emailController=TextEditingController();
+  var phoneController=TextEditingController();
+
+  SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text(
-        'Settings Screen',
-        style: Theme.of(context).textTheme.bodyText1 ,
-      ),
+    return BlocConsumer<ShopCubit,ShopStates>(
+      listener:(context,state)
+      {
+        if(state is ShopSuccessGetUserDataState)
+        {
+          nameController.text=state.loginModel.data!.name;
+          emailController.text=state.loginModel.data!.email!;
+          phoneController.text=state.loginModel.data!.phone;
+
+        }
+      },
+      builder: (context,state) {
+
+        var model=ShopCubit.get(context).userModel;
+        nameController.text=model?.data!.name;
+        emailController.text=model!.data!.email!;
+        phoneController.text=model.data!.phone;
+
+
+
+        return ConditionalBuilder(
+          condition: ShopCubit.get(context).userModel != null,
+
+          builder: (context)=>Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  if(state is ShopLoadingUpdateUserState)
+                  const LinearProgressIndicator(),
+
+                  const SizedBox(height: 10.0,),
+                  defaultFormField(
+                    controller: nameController,
+                    type: TextInputType.name,
+                    validate:(String value)
+                    {
+                      if(value.isEmpty){
+                        return 'Name Must Not Be Empty';
+                      }
+                      return null;
+                    },
+                    label:'Name',
+                    prefix:Icons.person,
+                  ),
+                  const SizedBox(height: 20.0,),
+                  defaultFormField(
+                    controller: emailController,
+                    type: TextInputType.emailAddress,
+                    validate:(String value)
+                    {
+                      if(value.isEmpty){
+                        return 'Email Must Not Be Empty';
+                      }
+                      return null;
+                    },
+                    label:'Email Address',
+                    prefix:Icons.email,
+                  ),
+                  const SizedBox(height: 20.0,),
+                  defaultFormField(
+                    controller: phoneController,
+                    type: TextInputType.phone,
+                    validate:(String value)
+                    {
+                      if(value.isEmpty){
+                        return 'Phone Must Not Be Empty';
+                      }
+                      return null;
+                    },
+                    label:'Phone',
+                    prefix:Icons.phone,
+                  ),
+                  const SizedBox(height: 20.0,),
+                  defaultButton(
+                    function: ()
+                    {
+                      if(formKey.currentState!.validate()){
+                        ShopCubit.get(context).updateUserData(
+                            name: nameController.text,
+                            email: emailController.text,
+                            phone: phoneController.text,
+                        );
+                      }
+
+                    },
+                    text:'UpDate',
+                  ),
+                  const SizedBox(height: 20.0,),
+                  defaultButton(
+                      function: ()
+                      {
+                        signOut(context);
+                      },
+                      text:'LOGOUT',
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+
+          fallback:(context)=>const Center(child: CircularProgressIndicator()) ,
+        );
+      },
     );
   }
 }
